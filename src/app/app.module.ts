ab52159ced7219428cb56e321206a927033c5075
Glo18 <kiogoraspecial@gmail.com>
2020-11-14 19:40:59 +0300

    add content in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFinderService } from './github-finder.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { GithubFinderService } from './github-finder.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [GithubFinderService],
   bootstrap: [AppComponent]
