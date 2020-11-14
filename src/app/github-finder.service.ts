25e05dd05b2cc3d532628ecdf876800c7031dae2
Glo18 <kiogoraspecial@gmail.com>
2020-11-14 17:10:58 +0300

    add httpClient

--- src/app/github-finder.service.ts
+++ src/app/github-finder.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubFinderService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getRepos(){
+    const username = 'Glo18';
+    return this.http.get('https://api.github.com/users/${username}/repos');
+  }
 }
