a95f9f43ec5af2c54574c249e895cf0cffbe65dc
Glo18 <kiogoraspecial@gmail.com>
2020-11-16 01:14:42 +0300

    add api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '20b066d8e8ecb2517a539ef223f4a9cc22b36a27'
 };
 
 /*
