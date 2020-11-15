8de044d990921307bcefd47a2378ee1981baaaac
Glo18 <kiogoraspecial@gmail.com>
2020-11-16 02:04:55 +0300

    add content in users class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    constructor(
+        public login: any,
+        public avatar_url: any,
+        public url: any,
+        public html_url: any,
+        public repos_url: any,
+        public followers_url: any,
+        public following_url: any,
+        public public_repos: any,
+        public created_at: any,
+       ){} 
 }
