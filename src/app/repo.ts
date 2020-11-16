c36a3810355c47c01844cd3a5b70bfe8e03bd31d
Glo18 <kiogoraspecial@gmail.com>
2020-11-16 23:22:58 +0300

    delete unnecessary content

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,9 +1,2 @@
 export class Repo {
-    constructor(
-        public name: any,
-        public description: any,
-        public full_name: any,
-        public html_url: any,
-        public language: any,
-    ){}
 }
